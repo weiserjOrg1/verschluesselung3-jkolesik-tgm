200b87a7a4a39c9dc0e4b05d160d410a6e01dfc3
Julian Kolesik <jkolesik@student.tgm.ac.at>
2018-10-19 16:55:45 +0200

    ShiftCipher implementiert mit Super-SecretAlphabet Änderung

---
+++ src/jkolesik/cipher/ShiftCipher.java
@@ -0,0 +1,53 @@
+package jkolesik.cipher;
+
+public class ShiftCipher extends MonoAlphabeticCipher{
+	/**
+	 * Konstruktor für die Klasse ShiftCipher	
+	 * @param i übernimmt, um wie viele Stellen die Buchstaben verschoben werden sollen
+	 */
+	public ShiftCipher(int i) {
+		cycle(i);
+	}
+	/**
+	 * die SetterMethode f�r den Shift
+	 * @param i �bernimmt, um wie viele Stellen die Buchstaben verschoben werden sollen
+	 */
+	public void setShiftValue(int i) {
+		if(i != 0) {
+			cycle(i);
+		}
+	}
+	/**
+	 * Diese Methode shiftet das Alphabet um soviele Stellen, wie es vom Benutzer eingegeben wurde
+	 * @param i �bernimmt, um wie viele Stellen die Buchstaben verschoben werden sollen
+	 */
+	private void cycle(int i){
+		char[] alph = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t',
+					   'u','v','w','x','y','z'};
+		char[] newAlph = new char[alph.length];
+		for(int j = 0; j < i; j++) {
+			newAlph[0] = alph[alph.length-1];
+			for(int x = 1; x < alph.length; x++) {
+				newAlph[x] = alph[x-1];
+			}
+			alph = newAlph;
+			if(j != i-1) {
+				newAlph = new char[alph.length];
+			}
+			
+		}
+		if(i != 0) {
+			String alphabet = "";
+			for(int y = 0; y < newAlph.length; y++) {
+				alphabet += newAlph[y]+"";
+			}
+			super.setSecretAlphabet(alphabet);
+		}else {
+			String alphabet = "";
+			for(int y = 0; y < alph.length; y++) {
+				alphabet += alph[y]+"";
+			}
+			super.setSecretAlphabet(alphabet);
+		}
+	}
+}
